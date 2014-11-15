d7a72d1a1aa63b81deb2a9e8c4e748d05b008786
Alessandro Desantis <desa.alessandro@gmail.com>
2014-11-15 19:25:41 +0100

    Add DB seeds

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :article do
-    title { Faker::Lorem.words(5).join(' ').capitalize }
-    body { Faker::Lorem.paragraph }
+    title { Faker::Lorem.words(3).join(' ').capitalize }
+    body { Faker::Lorem.paragraphs(20).join("\n\n") }
   end
 end
