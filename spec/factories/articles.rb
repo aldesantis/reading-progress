75bb6a9c34e328e89e43b638071fb50137693bb5
Alessandro Desantis <desa.alessandro@gmail.com>
2014-11-15 18:46:26 +0100

    Tweak generated article titles

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :article do
-    title { Faker::Lorem.sentence }
+    title { Faker::Lorem.words(5).join(' ').capitalize }
     body { Faker::Lorem.paragraph }
   end
 end
