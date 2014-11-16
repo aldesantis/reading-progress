1fd8f8be2852638570a7a86a3904d97725f9539a
Alessandro Desantis <desa.alessandro@gmail.com>
2014-11-16 11:08:56 +0100

    Add JS test for progress preservation

--- spec/features/articles/show_spec.rb
+++ spec/features/articles/show_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.feature 'The article page' do
-  scenario 'shows the article' do
-    article = FactoryGirl.create(:article)
+  given!(:article) { FactoryGirl.create(:article) }
+
+  background do
     visit root_path
     click_link article.title
+  end
+
+  scenario 'shows the article' do
     expect(page).to have_content(article.title)
+  end
+
+  scenario 'saves the reading progress', js: true do
+    expect {
+      page.execute_script 'window.scrollBy(0, 100)'
+      sleep 2.5
+    }.to change(article.positions, :count).by(1)
   end
 end
