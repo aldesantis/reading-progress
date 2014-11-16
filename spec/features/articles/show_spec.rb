2708117ac5da653a7a50dda10b273da7a8d13f19
Alessandro Desantis <desa.alessandro@gmail.com>
2014-11-16 12:18:08 +0100

    Test position loading

--- spec/features/articles/show_spec.rb
+++ spec/features/articles/show_spec.rb
@@ -5,16 +5,28 @@ RSpec.feature 'The article page' do
 
   background do
     visit root_path
-    click_link article.title
   end
 
   scenario 'shows the article' do
+    click_link article.title
     expect(page).to have_content(article.title)
   end
 
   scenario 'saves the reading progress', js: true do
+    click_link article.title
+
+    sleep 0.5
     page.execute_script 'window.scrollBy(0, 100)'
     sleep 2.5
+
     expect(page).to have_content('SENT!')
+  end
+
+  scenario 'loads the reading progress', js: true do
+    FactoryGirl.create(:position, article: article, offset: 200)
+
+    click_link article.title
+
+    expect(page).to have_content('LOADED!')
   end
 end
